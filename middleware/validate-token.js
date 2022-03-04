f2b2baf24c133c143a57d5bf45bf57a3a217fb60
leandroercoli <leandroercoli@gmail.com>
2022-03-04 14:52:56 -0300

    fix: verify function

--- middleware/validate-token.js
+++ middleware/validate-token.js
@@ -3,12 +3,12 @@ const jwt = require("jsonwebtoken");
 function validateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  
+
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
     if (err) return res.sendStatus(403);
-    req.user = user;
+    req.tokenData = decoded;
     next();
   });
 }
